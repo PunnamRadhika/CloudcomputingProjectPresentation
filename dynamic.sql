13f68a944e9319977f32d9dba92048f0e4a17d22
PunnamRadhika <128021244+PunnamRadhika@users.noreply.github.com>
2023-05-12 17:50:01 +0530

    Update dynamic.sql

--- dynamic.sql
+++ dynamic.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
 -- Generation Time: Mar 3, 2023 at 03:44 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
